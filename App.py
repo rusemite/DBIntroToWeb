506a243981bb88c03604b07337a8786dfba5693a
rusemite <imangazinr0304@gmail.com>
2024-11-10 15:46:30 +0500

    11nov

--- App.py
+++ App.py
@@ -1,31 +1,61 @@
 import streamlit as st
 from Pages import Home, Project1, Project2, Project3
-from streamlit_navigation_bar import st_navbar
+from streamlit_navigation_bar import st_navbar as navbar, st_navbar
+import os
+from PIL import Image
+import pandas as pd
+import numpy as np
 
-st.set_page_config(initial_sidebar_state="collapsed")
-pages = ['Home', 'Project1', 'Project2', 'Project3']
+image = Image.open('img/atletico.png')
+st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
+
+logo_path = os.path.join(os.path.dirname(__file__), "img", "atletico.svg")
+pages = [" ",'Home','Project1', 'Project2', 'Project3']
+
 styles = {
     "nav": {
-        "background-color": "rgb(102, 2, 60)",
+        "background-color": "rgb(102,2,60)",
+        "display": "flex",
+        "justify-content": "center"
     },
-    "div": {
+    "img": {
+        "position": "absolute",
+        "left": "-20px",
+        "font-size": "15px",
+        "top": "4px",
+        "width": "100px",
+        "height": "40px",
+    },
+
+"div": {
         "max-width": "32rem",
     },
+
     "span": {
         "border-radius": "0.5rem",
         "color": "rgb(0, 0, 255)",
         "margin": "0 0.125 rem",
         "padding": "0.4375rem 0.625rem",
     },
+
     "active": {
         "background-color": "rgba(105, 114, 255, 0.25)",
     },
     "hover": {
-        "background-color": "rgba(255, 255, 255, 0.35)"
+        "background-color": "rgb(255, 165, 0)"
     }
+
 }
 
-page = st_navbar(pages, styles=styles)
+options = {
+    "show_menu": False,
+    "show_sidebar": True,
+}
+
+page = st_navbar(pages,
+    styles=styles,
+    logo_path=logo_path,
+    options=options)
 
 if page == 'Home':
     Home.Home().app()
--- Pages/Project1.py
+++ Pages/Project1.py
@@ -1,7 +1,41 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 
 class Project1:
     def __int__(self):
         pass
     def app(self):
-        st.write('Project1')
+        st.title('Creation of DataFrame')
+
+        def load_data(file):
+            if file is not None:
+                data = pd.read_csv(file)
+                return data
+            return None
+
+        upload = st.file_uploader("Upload a CSV file")
+        if upload is not None:
+            df = load_data(upload)
+            st.dataframe(df, height=400, width=600)
+
+            column = st.selectbox("Choose a column for filtering", df.columns)
+
+            unique_values = df[column].unique()
+            selected_values = st.multiselect(f"Select values for {column}", options=unique_values,
+                                             default=unique_values)
+
+            filtered_df = df[df[column].isin(selected_values)]
+
+            st.dataframe(filtered_df, height=400, width=600)
+        else:
+            st.warning("Please upload a CSV file")
+
+        st.markdown("""<style>
+                            h1{
+                            color: Yellow;
+                            font-size:18px;
+                            text-align:center;
+                           }
+                            </style>""", unsafe_allow_html=True)
